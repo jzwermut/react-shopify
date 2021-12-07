4b343e20059195a3496ff5b503fc3bc69ef7c2f7
jzwermut <jzwermut@gmail.com>
2021-12-07 14:03:39 +1100

    button state change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Home from './pages/home'
 import Products from './pages/products'
 import Checkout from './pages/checkout'
 import Playground from './pages/playground'
+import Client from 'shopify-buy'
+import { client } from './helpers/helpers'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,24 +14,50 @@ import "tailwindcss/tailwind.css"
 
 const App = () => {
 
-const [cart, setCart] = useState([])
+const [cart, setCart] = useState({})
+
+const createCheckout = async () => {
+    const checkout = await client.checkout.create()
+    setCart(checkout)
+}
+
+const getCheckout = async(checkoutId) => {
+  const checkout = await client.checkout.fetch(checkoutId)
+  return checkout
+}
+
+const addToCart = async (checkoutId, lineItemsToAdd) => {
+    const add = await client.checkout.addLineItems(checkoutId, lineItemsToAdd)
+    const checkout =  await getCheckout(checkoutId)
+    setCart(checkout)
+}
+
+const removeFromCart = async (checkoutId, lineItemIds) => {
+    const remove = await client.checkout.removeLineItems(checkoutId, lineItemIds)
+    const checkout =  await getCheckout(checkoutId)
+    setCart(checkout)
+}
+
+useEffect(() => {
+    createCheckout()
+    },[]);
 
   return (
     <div>
-    <Router>
+      <Router>
         <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/shop">
-                <Products />
-              </Route>
-              <Route path="/checkout">
-                <Checkout />
-              </Route>
-              <Route path="/playground">
-                <Playground />
-              </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/shop">
+            <Products cart={cart} addToCart={addToCart} cartId={cart.id}/>
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/playground">
+            <Playground />
+          </Route>
         </Switch>
       </Router>
     </div>
--- src/pages/products.js
+++ src/pages/products.js
@@ -3,9 +3,27 @@ import Header from '../components/header'
 import Client from 'shopify-buy'
 import { client } from '../helpers/helpers'
 
-const Products = () => {
+const Products = ({cart, addToCart, cartId }) => {
 
 const [productList, setProductList] = useState([])
+
+const createLineItem = (variantId) => {
+	return [
+		{
+			variantId: variantId,
+			quantity: 1
+		}
+		];
+}
+
+const cartAdder = async (variantId, index) => {
+
+		const lineItems = await createLineItem(variantId);
+		addToCart(cartId, lineItems)
+		let newArr = [...productList]; // copying the old datas array
+  	newArr[index].clicked = true; // replace e.target.value with whatever you want to change it to
+  	setProductList(newArr);
+}
 
 const getProducts = async () => {
 
@@ -22,18 +40,31 @@ const getProducts = async () => {
 	  		href: '#',
 	  		price: '£' + p.variants[0].price,
 	  		imageSrc: p.images[0].src,
-	  		imageAlt: p.handle
+	  		imageAlt: p.handle,
+	  		variantId: p.variants[0].id,
+	  		clicked: false
 	  		}
 
 	  	listy.push(item)	
 
 	  })
-	 setProductList(listy)	
+	 setProductList(listy)
 	}
+
+const button = (clicked, vId, index) => {
+  if (clicked) {
+   return (<button className="w-full mt-2 text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">True</button>);
+  }
+  return (<button 
+  onClick={() => cartAdder(vId, index)}	
+  className="w-full mt-2 text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">False</button>);
+}
 
 useEffect(() => {
     getProducts()
   	},[]);
+
+
 
 	return (
 		<div>
@@ -42,7 +73,7 @@ useEffect(() => {
       			<div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         		<h2 className="sr-only">Products</h2>
 					<div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          			{productList.map((product) => (
+          			{productList.map((product, index) => (
             			<div key={product.id} href={product.href} className="group">
               				<div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
 				                <img
@@ -54,11 +85,13 @@ useEffect(() => {
               				<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
               				<p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
 
-              				  <button onClick={() => {console.log('add to cart clicked')}} type="button" class="w-full mt-2 text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              				{button(product.clicked, product.variantId, index)}
+              				
+              				  <button onClick={() => cartAdder(product.variantId, index)} type="button" 
+              				  className="w-full mt-2 text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center mr-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     										<svg className="-ml-1 mr-2 h-5 w-5 text-center" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
     										Buy now
   											</button>
-
             			</div>
           			))}
         			</div>
