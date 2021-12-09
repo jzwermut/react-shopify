464c57393d81ee38cc80d521b0bbe16aaebccfbe
jzwermut <jzwermut@gmail.com>
2021-12-10 09:23:19 +1100

    updated checkout

--- src/components/howitworks.js
+++ src/components/howitworks.js
@@ -5,26 +5,45 @@ const HowItWorks = () => {
 	return(
 		<div className="container mx-auto mt-8">
 
-			<div className="text-5xl text-center">How it works</div>
+			<div className="text-4xl text-purple-700 text-center text-opacity-75 font-sans">How it works</div>
 
 		<div className="flex flex-wrap justify-center mx-auto mt-3">
 
-			
+		<div className="m-5 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+		  <div className="md:flex">
+		    <div className="md:flex-shrink-0">
+		      <img className="h-48 w-full object-cover md:h-full md:w-48" src="/images/kicking.jpg" alt="Man looking at item at a store" />
+		    </div>
+		    <div className="p-8">
+		      <a href="#" className="block mt-1 text-2xl leading-tight font-medium text-black hover:underline">1. Choose a player and place your order.</a>
+		      <p className="mt-2 text-xl text-gray-500">Head on over to our shop to which players can sign your shirt.</p>
+		    </div>
+		  </div>
+		</div>
 
-			<div className="block mx-3 p-6 bg-white hover:bg-gray-100 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 sm: my-3">
-    			<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">1. Choose a player and place your order.</h5>
-    			<p className="font-normal text-gray-700 dark:text-gray-400">Head on over to our shop to which players can sign your shirt.</p>
-			</div>
+		<div className="m-5 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+		  <div className="md:flex">
+		    <div className="md:flex-shrink-0">
+		      <img className="h-48 w-full object-cover md:h-full md:w-48" src="/images/post.jpg" alt="Man looking at item at a store" />
+		    </div>
+		    <div className="p-8">
+		      <a href="#" className="block mt-1 text-2xl leading-tight font-medium text-black hover:underline">2. Send your existing shirt in.</a>
+		      <p className="mt-2 text-xl text-gray-500">We'll get your favourite Spurs shirt signed.</p>
+		    </div>
+		  </div>
+		</div>
 
-			<div className="block mx-3 p-6 bg-white hover:bg-gray-100 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 sm: my-3">
-    			<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">2. Send your existing shirt in.</h5>
-    			<p className="font-normal text-gray-700 dark:text-gray-400">We'll get your favourite Spurs shirt signed.</p>
-			</div>
-
-			<div className="block mx-3 p-6 bg-white hover:bg-gray-100 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 sm: my-3">
-    			<h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">3. We'll get it signed and send it back.</h5>
-    			<p className="font-normal text-gray-700 dark:text-gray-400">Once we receive your shirt we'll have it signed and send it back within 20 days.</p>
-			</div>
+		<div className="m-5 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+		  <div className="md:flex">
+		    <div className="md:flex-shrink-0">
+		      <img className="h-48 w-full object-cover md:h-full md:w-48" src="/images/overit.jpg" alt="Man looking at item at a store" />
+		    </div>
+		    <div className="p-8">
+		      <a href="#" className="block mt-1 text-2xl leading-tight font-medium text-black hover:underline">3. We'll get it signed and send it back.</a>
+		      <p className="mt-2 text-xl text-gray-500">Once we receive your shirt we'll have it signed and send it back within 20 days.</p>
+		    </div>
+		  </div>
+		</div>
 
 		</div>
 
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -18,6 +18,7 @@ const [lastName,setLastName] = useState('')
 const [phone,setPhone] = useState('')
 const [state,setState] = useState('')
 const [postcode,setPostcode] = useState('')
+const [open,setOpen] = useState(false)
 
 const handleSubmit = async (e) => {
   e.preventDefault()
@@ -40,57 +41,108 @@ const handleSubmit = async (e) => {
  window.location.href = `${cart.webUrl}`
 }
 
+if (cart.lineItems === undefined ) {
+  return (
+  <h1>...Loading</h1>
+  )
+}
+else {
+
+
 	return (
   <div>
     <Header cart={cart}  removeItem={removeItem} productList={productList}/>
-    <div className="container mx-auto p-20">
-      <div className="text-center font-large mb-10"> Shipping deets</div>
+
+    <div className="container mx-auto flex flex-wrap grid grid-cols-3 p-20">
+      <div className="text-center text-2xl mb-10 col-span-2 px-5"> Shipping details
     
       <form onSubmit={handleSubmit}>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 1</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 1</label>
           <input onChange={(e) => setAdd1(e.target.value)} type="address" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='1 George St' />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 2</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 2</label>
           <input onChange={(e) => setAdd2(e.target.value)} type="address" id="addressLine2" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Apartment 2' />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">City</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">City</label>
           <input onChange={(e) => setCity(e.target.value)} type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Sydney' />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Country</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">Country</label>
           <input onChange={(e) => setCountry(e.target.value)} type="text" id="country" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Australia' />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">First Name</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">First Name</label>
           <input onChange={(e) => setFirstName(e.target.value)} type="text" id="firstname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Bobby' />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Last Name</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">Last Name</label>
           <input onChange={(e) => setLastName(e.target.value)} type="text" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Brady' />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Phone</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">Phone</label>
           <input onChange={(e) => setPhone(e.target.value)} type="number" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='0412 345 678'  />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">State</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">State</label>
           <input onChange={(e) => setState(e.target.value)} type="text" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='NSW'  />
         </div>
         <div className="mb-6">
-          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Postcode</label>
+          <label for="addressLine1" className="text-sm text-left font-medium text-gray-900 block mb-2 dark:text-gray-300">Postcode</label>
           <input onChange={(e) => setPostcode(e.target.value)} type="number" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='2000'  />
         </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Submit
         </button>
       </form>
 
+      </div>
+
+                  <div className="text-2xl flex-none text-center col-span-1 bg-blue-800 bg-opacity-25 rounded-lg">Cart
+                    <div className="mt-8 px-5 rounded">
+                      <div className="flow-root">
+                        <ul role="list" className="-my-6 divide-y divide-gray-200 bg-white rounded-lg">
+                          {cart.lineItems.map((item) => (
+                            <li key={item.id} className="py-6 flex">
+                              <div className="flex-shrink-0 w-24 h-24 bg-white border border-gray-200 rounded-md overflow-hidden">
+                                <img
+                                  src={item.variant.image.src}
+                                  alt={item.title}
+                                  className="w-full h-full object-center object-cover"
+                                />
+                              </div>
+
+                              <div className="ml-4 flex-1 flex flex-col">
+                                <div>
+                                  <div className="flex justify-between text-base font-medium text-gray-900">
+                                    <h3>
+                                      <a href={item.href}>{item.title}</a>
+                                    </h3>
+                                    <p className="ml-4">{item.variant.price}</p>
+                                  </div>
+                                  <p className="mt-1 text-sm text-gray-500">Signed shirt</p>
+                                </div>
+                                <div className="flex-1 flex items-end justify-between text-sm">
+                                  <p className="text-gray-500">Qty {item.quantity}</p>
+                                </div>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  
+
+
     </div>
+    
   </div>
-  )
+    )
+  }
 }
 
 export default Checkout;
+
