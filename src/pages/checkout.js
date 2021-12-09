344b43eab6925a2223f48c5e183bfaffdea3398c
jzwermut <jzwermut@gmail.com>
2021-12-09 20:36:09 +1100

    markdown

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -37,52 +37,53 @@ const handleSubmit = async (e) => {
 
  const checkout = await client.checkout.updateShippingAddress(cart.id, shippingAddress)
 
-  window.location.href = `${cart.webUrl}`
+ window.location.href = `${cart.webUrl}`
 }
 
 	return (
   <div>
     <Header cart={cart}  removeItem={removeItem} productList={productList}/>
-    <div class="container mx-auto p-20">
+    <div className="container mx-auto p-20">
+      <div className="text-center font-large mb-10"> Shipping deets</div>
     
       <form onSubmit={handleSubmit}>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 1</label>
-          <input onChange={(e) => setAdd1(e.target.value)} type="address" id="addressLine1" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='1 George St' />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 1</label>
+          <input onChange={(e) => setAdd1(e.target.value)} type="address" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='1 George St' />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 2</label>
-          <input onChange={(e) => setAdd2(e.target.value)} type="address" id="addressLine2" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Apartment 2' />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Address line 2</label>
+          <input onChange={(e) => setAdd2(e.target.value)} type="address" id="addressLine2" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Apartment 2' />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">City</label>
-          <input onChange={(e) => setCity(e.target.value)} type="text" id="city" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Sydney' />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">City</label>
+          <input onChange={(e) => setCity(e.target.value)} type="text" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Sydney' />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Country</label>
-          <input onChange={(e) => setCountry(e.target.value)} type="text" id="country" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Australia' />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Country</label>
+          <input onChange={(e) => setCountry(e.target.value)} type="text" id="country" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Australia' />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">First Name</label>
-          <input onChange={(e) => setFirstName(e.target.value)} type="text" id="firstname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Bobby' />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">First Name</label>
+          <input onChange={(e) => setFirstName(e.target.value)} type="text" id="firstname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Bobby' />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Last Name</label>
-          <input onChange={(e) => setLastName(e.target.value)} type="text" id="lastname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Brady' />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Last Name</label>
+          <input onChange={(e) => setLastName(e.target.value)} type="text" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Brady' />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Phone</label>
-          <input onChange={(e) => setPhone(e.target.value)} type="number" id="addressLine1" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='0412 345 678'  />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Phone</label>
+          <input onChange={(e) => setPhone(e.target.value)} type="number" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='0412 345 678'  />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">State</label>
-          <input onChange={(e) => setState(e.target.value)} type="text" id="addressLine1" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='NSW'  />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">State</label>
+          <input onChange={(e) => setState(e.target.value)} type="text" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='NSW'  />
         </div>
-        <div class="mb-6">
-          <label for="addressLine1" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Postcode</label>
-          <input onChange={(e) => setPostcode(e.target.value)} type="number" id="addressLine1" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='2000'  />
+        <div className="mb-6">
+          <label for="addressLine1" className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Postcode</label>
+          <input onChange={(e) => setPostcode(e.target.value)} type="number" id="addressLine1" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='2000'  />
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Submit
         </button>
       </form>
